999540b8c6048c487fe6064fd13746c8220d930d
Gustavo Sepulveda <gustavo.sepulvedaq@gmail.com>
2024-07-10 22:34:29 -0400

    Código base de proyecto Viajes Chile

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#enviarCorreo").click(function () {
+        alert("El correo fue enviado correctamente...");
+    });
+
+});
+
+var tooltipTriggerLis=[] .slice.call(document.querySelectorA11('[data-bs-toggle="msj"]'))
+var tooltipList=tooltipTriggerList.map(function(tooltipTriggerE1){
+return new bootstrap.Tooltip(tooltipTriggerE1)
+})
